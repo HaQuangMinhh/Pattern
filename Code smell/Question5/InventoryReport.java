ecee3fc7114bbfd5fe93ea1ff607b5ff0e7820d4
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2025-03-13 15:12:15 +0700

    Add files via upload

---
+++ Code smell/Question5/InventoryReport.java
@@ -0,0 +1,8 @@
+public class InventoryReport implements Report {
+
+    @Override
+    public void generate() {
+        System.out.println("Inventory report generated");
+    }
+
+}
---
+++ Code smell/Question5/SalesReport.java
@@ -0,0 +1,10 @@
+public class SalesReport implements Report {
+
+    @Override
+    public void generate() {
+        System.out.println("Sales report generated");
+        
+    }
+
+
+}
